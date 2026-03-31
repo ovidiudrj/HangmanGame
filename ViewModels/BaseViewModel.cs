8ad3c1bebf9e284f58d985396ca498bc50b68510
Dirjan Valentin Ovidiu <darjanovidiu64@gmail.com>
2026-03-31 11:48:57 +0300

    create MVVM folder structure & base classes

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace HangmanGame.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
