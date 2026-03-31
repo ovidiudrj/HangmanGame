a3e6ddaaa4ccba35952c5662d52322f8c8657fd6
Dirjan Valentin Ovidiu <darjanovidiu64@gmail.com>
2026-03-31 11:50:51 +0300

    add UserModel with Name and  ImagePath

---
+++ Models/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HangmanGame.ViewModels;
+
+namespace HangmanGame.Models
+{
+    public class UserModel : BaseViewModel
+    {
+        private string _name;
+        private string _imagePath;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string ImagePath
+        {
+            get => _imagePath;
+            set
+            {
+                _imagePath = value;
+                OnPropertyChanged(nameof(ImagePath)); 
+            }
+        }
+    }
+}
